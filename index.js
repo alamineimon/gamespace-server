9746bfa5d47886ec53eccff1bfa948d275e533c2
sumanta62 <sumantamajumder62@gmail.com>
2023-02-08 22:43:23 +0600

    html game Section update

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ async function run() {
     const gamesCollection = client.db("GameSpace").collection("games");
     const gamesComment = client.db("GameSpace").collection("comment");
     const orderedGameCollection = client.db("GameSpace").collection("orderedGames");
-    const activePlayerProfile = client.db("GameSpace").collection("activeProfile");
 
     // get users
 
@@ -106,21 +105,8 @@ async function run() {
       const result = await usersCollection.insertOne(data);
       res.send(result);
     });
-    // Active Players
-    app.post("/activePlayres", async (req, res) =>{
-      const data = req.body;
-      const result = await activePlayerProfile.insertOne(data)
-      res.send(result);
-    })
-    app.get("/activePlayres", async (req, res) => {
-       const query = req.body;
-      console.log(query)
-      const activePlayres = await activePlayerProfile.find(query).toArray();
-      console.log(activePlayres)
-      res.send(activePlayres);
-    });
-
-
+  
+    
     app.post("/user", async (req, res) => {
       const data = req.body;
       const result = await usersCollection.insertOne(data);
